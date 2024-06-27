be91e1d1bad5e98ee2433710667d32de6f97ce54
ismailemayej <salikbd.com@gmai.com>
2024-06-28 00:54:44 +0600

    error handle

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -19,9 +19,7 @@ const Blogs = async () => {
         className="grid relative px-2 lg:grid-cols-4 w-[100%] md:grid-cols-2 grid-cols-1 pb-12 gap-2"
       >
         {data?.data?.map((news: any) => (
-          <div>
-            <BlogCard key={news._id} news={news} />
-          </div>
+          <BlogCard key={news._id} news={news} />
         ))}
       </div>
       <span className="flex justify-end w-full">
--- components/main/Allblogs.tsx
+++ components/main/Allblogs.tsx
@@ -18,9 +18,7 @@ const Allblog = async () => {
         className="grid relative px-2 lg:grid-cols-4 w-[100%] md:grid-cols-2 grid-cols-1 pb-12 gap-2"
       >
         {data?.data?.map((news: any) => (
-          <div>
-            <BlogCard key={news._id} news={news} />
-          </div>
+          <BlogCard key={news._id} news={news} />
         ))}
       </div>
     </>
